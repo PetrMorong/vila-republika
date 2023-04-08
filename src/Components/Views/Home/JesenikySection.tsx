37112469985039814af7b7a1b16cf85f159e61cf
Macbook <moriandr73@gmail.com>
2023-04-08 16:26:29 +0200

    new photo jeseniky

--- src/Components/Views/Home/JesenikySection.tsx
+++ src/Components/Views/Home/JesenikySection.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { Button } from "../../../Styled/Button.styles";
-import { NostalgickWrapper } from "../../../Styled/Nostalgick.styles";
-import { FlexRow } from "../../../Styled/Theme.styles";
-import { Desc, SectionTitle } from "../../../Styled/Typography.styles";
-import styled from "styled-components";
-import { Link } from "gatsby";
-import { buildImageUrl } from "cloudinary-build-url";
+import React from 'react'
+import { Button } from '../../../Styled/Button.styles'
+import { NostalgickWrapper } from '../../../Styled/Nostalgick.styles'
+import { FlexRow } from '../../../Styled/Theme.styles'
+import { Desc, SectionTitle } from '../../../Styled/Typography.styles'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+import { buildImageUrl } from 'cloudinary-build-url'
 
 export const Container = styled.div`
   display: flex;
@@ -14,23 +14,20 @@ export const Container = styled.div`
   position: relative;
   z-index: 10;
   height: 100%;
-`;
+`
 
 const Nostalgick: React.FC = () => {
-  const bg = buildImageUrl(
-    "v1673867133/vilaRepublikaFoto/nostalgick-bg_psvvlh.png",
-    {}
-  );
+  const bg = buildImageUrl('v1680961843/vilaRepublikaFoto/5_1_hfljc1.jpg', {})
   return (
     <NostalgickWrapper padding={`0px 0px`} height={``} bg={bg}>
       <Container>
         <FlexRow maxWidth={`690px`} margin={`0px auto`} column>
           <SectionTitle color={`white`} mb={`40px`}>
-            Nostalgické kouzlo hrubého Jeseníku{" "}
+            Nostalgické kouzlo hrubého Jeseníku{' '}
           </SectionTitle>
           <Desc
-            color={"white"}
-            mb={"20px"}
+            color={'white'}
+            mb={'20px'}
             mdFontsize={`18px`}
             smMargin={`26px`}
             smFontsize={`18px`}
@@ -44,22 +41,22 @@ const Nostalgick: React.FC = () => {
             na vás, zda pěšky, na kole či na lyžích.
           </Desc>
           <div style={{ marginBottom: 30 }} />
-          <Link to="/okoli" style={{ margin: "auto" }}>
+          <Link to="/okoli" style={{ margin: 'auto' }}>
             <Button
-              style={{ width: "176px" }}
+              style={{ width: '176px' }}
               margin={`auto`}
               width={`176px`}
               height={`61px`}
               brColor={`white`}
               bgColor={`transparent`}
             >
-              VÍCE O OKOLÍ{" "}
+              VÍCE O OKOLÍ{' '}
             </Button>
           </Link>
         </FlexRow>
       </Container>
     </NostalgickWrapper>
-  );
-};
+  )
+}
 
-export default Nostalgick;
+export default Nostalgick
